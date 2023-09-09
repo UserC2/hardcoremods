d91b38a5c61a0e24f33a9ffe56cf120f36306589
UserC2 <85082371+UserC2@users.noreply.github.com>
2023-09-09 18:15:33 -0400

    Set branch name

--- Update_Mods.bat
+++ Update_Mods.bat
@@ -1,7 +1,7 @@
-REM CHANGE "BRANCH_NAME" AND "YOUR_USERNAME" to the correct server and your username before running!
+REM CHANGE "YOUR_USERNAME" to your username before running!
 REM If Minecraft is installed someplace else, modify mod_dir.
 
-SET branch = "BRANCH_NAME"
+SET branch = "series"
 SET mod_dir="C:\Users\YOUR_USERNAME\AppData\Roaming\.minecraft\mods"
 
 cd %mod_dir%
