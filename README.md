088b08a53235fe15737943b93b263f12b1f1e8b1
KRcopy <elandyg1@gmail.com>
2023-09-17 17:22:44 -0400

    added untracked mods and patchouli

--- README.md
+++
@@ -1,3 +0,0 @@
-# Branch Specific Modifications
-
-Download [Forge 43.2.3](https://maven.minecraftforge.net/net/minecraftforge/forge/1.19.2-43.2.3/forge-1.19.2-43.2.3-installer.jar). Earlier versions of Forge for 1.19.2 will not work with the mod Create.
--- Update_Mods.bat
+++
@@ -1,9 +0,0 @@
-REM CHANGE "YOUR_USERNAME" to your username before running!
-REM If Minecraft is installed someplace else, modify mod_dir.
-
-SET branch = "series"
-SET mod_dir="C:\Users\YOUR_USERNAME\AppData\Roaming\.minecraft\mods"
-
-cd %mod_dir%
-git checkout %branch%
-git pull
